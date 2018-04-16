9d40c770bf445adf67adf974371384b00a596b42
nato <natoszmedra@gmail.com>
2018-04-16 17:39:41 -0300

    Creo el planificador

---
+++ planificador/socket.h
@@ -0,0 +1,17 @@
+/*
+ * socket.h
+ *
+ *  Created on: 16 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <string.h>
+	#include <arpa/inet.h>
+	#include <sys/socket.h>
+
+#endif /* SOCKET_H_ */
