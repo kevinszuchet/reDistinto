12bb8c252daf18f815471e3f1a4c1e1ae212ff70
nato <natoszmedra@gmail.com>
2018-04-16 17:28:10 -0300

    Commit inicial con el servidor del coordinador

---
+++ coordinador/sockets/socket.h
@@ -0,0 +1,16 @@
+/*
+ * socket.h
+ *
+ *  Created on: 16 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+	#include <stdio.h>
+	#include <arpa/inet.h>
+	#include <sys/socket.h>
+	#include <errno.h>
+
+#endif /* SOCKET_H_ */
