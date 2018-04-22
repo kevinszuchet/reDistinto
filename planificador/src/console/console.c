ec4baeffe713c02c5cc445ca77f9e83087c3dcb7
fedekiwo <fedekiwo@gmail.com>
2018-04-22 15:27:28 -0300

    subo los cambios de la consola

--- planificador/src/console/console.c
+++ planificador/src/console/console.c
@@ -5,7 +5,7 @@
  *      Author: utnso
  */
 
-#include "console.h"
+
 
 void openConsole() {
 	char *line;
@@ -29,21 +29,26 @@ void openConsole() {
 }
 
 void execute(char *command) {
-	printf("%s", command);
-	switchs(command) {
-		icases("pause")
-		icases("continue")
-		icases("block")
-		icases("unblock")
-		icases("list")
-		icases("kill")
-		icases("status")
-		icases("deadlock")
-			printf("El comando ingresado fue: %s\n", command);
-			break;
 
-		defaults
-			printf("El comando %s no es valido\n", command);
-			break;
-	} switchs_end;
+	if(!strcmp(command, "pause")){
+
+	} else if(strcmp(command, "continue")){
+
+	} else if(strcmp(command, "block")){
+
+	} else if(strcmp(command, "unblock")){
+
+	} else if(strcmp(command, "list")){
+
+	} else if(strcmp(command, "kill")){
+
+	} else if(strcmp(command, "status")){
+
+	} else if(strcmp(command, "deadlock")){
+
+	} else {
+		printf("%s: command not found.", command);
+	}
+
 }
+
