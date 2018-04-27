963e9322af1c633227dc73f9b57ae2c98f25d4b0
MarianoAlvarezPerez <marianoalvarez3p@gmail.com>
2018-04-27 14:08:39 -0300

    Added operation codes in our library

---
+++ our-commons/our-commons/messages/operation_codes.h
@@ -0,0 +1,25 @@
+/*
+ * operation_codes.h
+ *
+ *  Created on: 27 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef OPERATION_CODES_H_
+#define OPERATION_CODES_H_
+
+#define EXITO '1'
+#define FINALIZADO '2'
+#define FALLA '3'
+#define SET '4'
+#define GET '5'
+#define RUN '6'
+#define COMPACT '7'
+#define STORE '8'
+#define LOCK '9'
+#define ERROR_CLAVE_SIZE '0'
+#define ERROR_CLAVE_NOT_IDENTIFIED 'q'
+#define ERROR_CLAVE_NON_EXISTENT 'w'
+#define ERROR_CLAVE_NOT_FOUND 'e'
+
+#endif /* OPERATION_CODES_H_ */
