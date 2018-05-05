f29bfcff59b4a34ac4849da424fc399e188749d8
fedekiwo <fedekiwo@gmail.com>
2018-05-05 00:30:01 -0300

    Prototipos de funciones + principio de implementación en la instancia

--- instancia/src/instancia.c
+++ instancia/src/instancia.c
@@ -8,7 +8,9 @@
 #include "instancia.h"
 
 t_log * logger;
-
+int entryAmount;
+int entrySize;
+t_dictionary * keyTable; //Takes record of the key + how many entraces the value occupies
 int main(void) {
 	logger = log_create("../instancia.log", "tpSO", true, LOG_LEVEL_INFO);
 	char* ipCoordinador;
@@ -24,10 +26,11 @@ int main(void) {
 	printf("Path = %s\n", path);
 	printf("Name= %s\n", name);
 	printf("Dump= %d\n", dump);
-
+	log_info(logger, "trying to connect to coordinador...\n");
 	int coordinadorSocket = connectToServer(ipCoordinador, portCoordinador, COORDINADOR, INSTANCIA, logger);
 	if (coordinadorSocket < 0){
 		//reintentar conexion?
+		log_error(logger, "An error has occurred while trying to connect to coordinador\n socket number: %d\n", coordinadorSocket);
 		return -1;
 	}
 
@@ -48,5 +51,78 @@ void getConfig(char** ipCoordinador, int* portCoordinador, char** algorithm, cha
 	*dump = config_get_int_value(config, "DUMP");
 }
 
-void initialize(int entryAmount, int entrySize){
+int initialize(int entraces, int storage){
+	//Que casos de error puede haber?? Pensarlo.
+	entryAmount = entraces;
+	entrySize = storage;
+	keyTable = dictionary_creat();//tendría que tocar las so-commons para hacer un dictionary_create que reciba un
+								 //int como parametro y ese int sea la máxima cantidad de entradas así no es por default
+	//Pensar bien como se crea la table de clave-valor
+	log_info(logger, "Instancia was intialized correctly\n");
+	return 0;
 }
+int set(char *key, char *value){
+	int freeStorage;
+	int entriesForValue;
+	if((dictionary_has_key(keyTable, key),"true")){
+		updateKey(key, value);
+	}
+	log_info(logger, "Total free storage: %d", getFreeStorage());
+	log_info(logger, "Total size of value: %d", sizeof(value));
+	log_info(logger, "Total free contiguous free entriens: %d", getcontiguousFreeEntries());
+	entriesForValue = sizeof(value) / entrySize;
+	log_info(logger, "Total entries for value: %d", entriesForValue);
+	if(getFreeStorage() < sizeof(value)){
+	    log_info(logger, "there is not enough space to set the key: %s", key);
+		//borro alguna key según algoritmo y setteo
+
+	}
+	if(getContiguousFreeEntries() < entriesForValue){
+		//tengo que compactar
+	}
+
+	//hago el set.
+	log_info(logger, "Set operation for key: %s and value: %s, was successfully done\n", key, value);
+	return 0;
+}
+int notifyCoodinador(char *key, char *value, char *operation){
+	log_info(logger, "%s operation, with key: %s and value: %s, was succesffully notified to coordinador\n", operation, key, value);
+	return 0;
+}
+int dump(){
+	log_info(logger, "Dump was succesffully done\n");
+	return 0;
+}
+int compact(){
+	log_info(logger, "Compactation was successfully done\n");
+	return 0;
+}
+int updateKey(char *key, char *value){
+	log_info(logger, "The key: %s, already exists so it will be updated with value: %s\n", key, value);
+	//...
+	log_info(logger, "The key: %s, was successfully updated with the value: %s\n", key, value);
+	return 0;
+}
+int store(char *key){
+	log_info(logger, "The key: %s, was succesfully stored/n", key);
+	return 0;
+}
+int finish(){
+	//dictionary_clean_and_destroy_elements(keyTable, ); Averiguar bien que es lo que le tengo que pasar como segundo parametro
+	log_info(logger, "Instancia was finished correctly, bye bye, it was a pleasure!!\n");
+	return 0;
+}
+int getAmountOfFreeEntraces(){
+	int amount = 0;
+	log_info(logger, "There are %d free spaces\n", amount);
+	return amount;
+}
+int getFreeStorage(){
+	return getAmountOfFreeEntraces() * entrySize;
+}
+int getMaxContiguousFreeEtries(){
+	int max = 0;
+	log_info(logger, "There are %d contiguous free entries\n", max);
+	return max;
+}
+
--- instancia/src/instancia.h
+++ instancia/src/instancia.h
@@ -13,6 +13,7 @@
 	#include <commons/string.h>
 	#include <commons/config.h>
 	#include <commons/log.h>
+	#include <commons/collections/dictionary.h>
 	#define  CFG_FILE "../instancia.cfg"
 
 	void getConfig(char** ipCoordinador, int* portCoordinador, char** algorithm, char** path, char** name, int* dump);
