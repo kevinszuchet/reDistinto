0f431e5b4a7aa66c4219bb7875f8298d98e591d7
nato <natoszmedra@gmail.com>
2018-06-07 15:12:07 -0300

    Saco del gitignore los .d y .o para poder compilar desde la vm server. Como esos archivos los genera eclipse, si no se suben al repo no se va a poder compilar sin haber compilado con eclipse

---
+++ coordinador/Debug/src/submodules/instancia/instanciaFunctions.d
@@ -0,0 +1,41 @@
+src/submodules/instancia/instanciaFunctions.o: \
+ ../src/submodules/instancia/instanciaFunctions.c \
+ ../src/submodules/instancia/instanciaFunctions.h \
+ ../src/submodules/instancia/../../coordinador.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/server.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/sockets.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/modules/names.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/serialization.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/tads.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/../messages/operation_codes.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../sockets/sockets.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/tads.h \
+ ../src/submodules/instancia/../../submodules/instancia/instanciaFunctions.h
+
+../src/submodules/instancia/instanciaFunctions.h:
+
+../src/submodules/instancia/../../coordinador.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/server.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/sockets.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/modules/names.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/serialization.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/tads.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/../messages/operation_codes.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../sockets/sockets.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/tads.h:
+
+../src/submodules/instancia/../../submodules/instancia/instanciaFunctions.h:
---
+++ esi/Debug/src/esi.d
@@ -0,0 +1,30 @@
+src/esi.o: ../src/esi.c ../src/esi.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/client.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/sockets.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/modules/names.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/tads.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/../messages/operation_codes.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/serialization.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/tads.h \
+ /home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../sockets/sockets.h
+
+../src/esi.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/client.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/sockets/sockets.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/modules/names.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/tads.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/tads/../messages/operation_codes.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/serialization.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/operation_codes.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../tads/tads.h:
+
+/home/utnso/workspace/tp-2018-1c-youKnowNothing/our-commons/our-commons/messages/../sockets/sockets.h:
